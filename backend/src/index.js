05ee819108912fd7bf288c9db1d6a5aa09975df3
X-Engineer-001 <fabin1790@gmail.com>
2023-04-08 20:14:29 +0800

    database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import rootRouter from "./routes";
+import { prisma } from "./adapters";
 const port = process.env.PORT || 8000;
 const app = express();
 app.use(rootRouter);
@@ -8,4 +9,7 @@ res.send("Hello World!");
 });
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`);
+});
+process.on("exit", async () => {
+await prisma.$disconnect();
 });
